6f24fb9b3e1276c403b943c48fc9b8bf96c25405
Umidjon <mrumidjon2020@gmail.com>
2022-11-13 03:38:14 +0500

    wikipedia summary sentences=1

--- 02-wikibot/wikitest.py
+++ 02-wikibot/wikitest.py
@@ -2,8 +2,4 @@ import wikipedia
 
 wikipedia.set_lang('uz')
 
-# print(wikipedia.search('Sharqiy Turkiston'))
-response = wikipedia.summary('odam')
-
-for x in range(0, len(response), 4095):
-    print(response[x:x+4095])
+print(wikipedia.summary('odam', sentences=10))
