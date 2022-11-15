1b4ce2bf7e125cde29ee547b446538eaab336a37
Umidjon <mrumidjon2020@gmail.com>
2022-11-16 03:13:30 +0500

    Quran Api

--- 03-speakEnglishBot/APIs/quranApi.py
+++ 03-speakEnglishBot/APIs/quranApi.py
@@ -2,11 +2,11 @@ import requests
 from pprint import pprint as print
 
 sura = 1
-oyat = 1
+oyat = 7
 tafsir = "uzb-muhammadsodikmu"
 
-url_oyat = f"https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/{tafsir}/5/10.json"
-url_sura = f"https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/{tafsir}/5.json"
+url_oyat = f"https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/{tafsir}/{sura}/{oyat}.json"
+url_sura = f"https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/{tafsir}/{sura}.json"
 response = requests.get(url_oyat)
 print(response.status_code)
 
