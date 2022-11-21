1879e8ba5b275ebd62bd53d7b3c32e0caab81d2a
Umidjon <mrumidjon2020@gmail.com>
2022-11-21 23:37:51 +0500

    added uzwords

--- 04-imloBot/check-words.py
+++ 04-imloBot/check-words.py
@@ -1,6 +1,17 @@
 from uzwords import words
+from difflib import get_close_matches
 
 print(len(words))
 print(words[0])
 print(words[-1])
 print(words[23233])
+print('-' * 100)
+
+print(get_close_matches("ҳато", words))
+print(get_close_matches("тариҳ", words))
+print(get_close_matches("хато", words))
+print(get_close_matches("олма", words))
+print(get_close_matches("ҳат", words))
+print(get_close_matches("ҳайт", words))
+print('-' * 100)
+
