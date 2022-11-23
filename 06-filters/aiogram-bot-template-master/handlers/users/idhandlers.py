ca1a3fed9229f0f02d004b89ab0bc22db8636678
Umidjon <mrumidjon2020@gmail.com>
2022-11-23 23:29:11 +0500

    id handlers

--- 06-filters/aiogram-bot-template-master/handlers/users/idhandlers.py
+++ 06-filters/aiogram-bot-template-master/handlers/users/idhandlers.py
@@ -7,7 +7,7 @@ SUPER_USERS = [2007427, 1634251726]
 BLACK_LIST = [1234567, 5687981]
 
 
-@dp.message_handler(filters.IDFilter(chat_id=SUPER_USERS))
+# @dp.message_handler(filters.IDFilter(chat_id=SUPER_USERS))
 # @dp.message_handler(chat_id=SUPER_USERS, text='secret')
 @dp.message_handler(chat_id=SUPER_USERS, commands='start')
 async def id_filter_handler(msg: types.Message):
