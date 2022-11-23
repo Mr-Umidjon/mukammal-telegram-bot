ea9e515a6cbdf6c40d635c465ecf2d60b5d3556c
Umidjon <mrumidjon2020@gmail.com>
2022-11-23 19:42:18 +0500

    filters

--- 06-filters/aiogram-bot-template-master/handlers/users/start.py
+++ 06-filters/aiogram-bot-template-master/handlers/users/start.py
@@ -4,14 +4,6 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 from loader import dp
 
 
-@dp.message_handler(CommandStart(deep_link='kunuz'))
-async def bot_start(message: types.Message):
-    args = message.get_args()
-    text = f"Salom, {message.from_user.full_name}!\n"
-    text += f"Sizni {args} tavsiya qildi"
-    await message.answer(f"Salom, {message.from_user.full_name}!")
-
-
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!")
