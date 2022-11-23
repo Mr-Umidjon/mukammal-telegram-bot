cb0156751d7a6ab3f9afc5adf7808e5c1809c363
Umidjon <mrumidjon2020@gmail.com>
2022-11-23 21:59:36 +0500

    help

--- 06-filters/aiogram-bot-template-master/handlers/users/help.py
+++ 06-filters/aiogram-bot-template-master/handlers/users/help.py
@@ -1,5 +1,5 @@
 from aiogram import types
-from aiogram.dispatcher.filters.builtin import CommandHelp
+from aiogram.dispatcher.filters.builtin import CommandHelp, CommandSettings
 
 from loader import dp
 
@@ -9,5 +9,11 @@ async def bot_help(message: types.Message):
     text = ("Buyruqlar: ",
             "/start - Botni ishga tushirish",
             "/help - Yordam")
-    
+
     await message.answer("\n".join(text))
+
+
+@dp.message_handler(CommandSettings())
+async def bot_settings(message: types.Message):
+    text = "Sozlamlar"
+    await message.answer(text)
