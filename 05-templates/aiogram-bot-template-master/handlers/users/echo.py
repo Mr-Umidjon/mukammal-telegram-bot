e1caa7f501df24ef99f053cc06f3b3b128b82a4b
Umidjon <mrumidjon2020@gmail.com>
2022-11-23 14:39:59 +0500

    wiki bot added

--- 05-templates/aiogram-bot-template-master/handlers/users/echo.py
+++
@@ -1,9 +0,0 @@
-from aiogram import types
-
-from loader import dp
-
-
-# Echo bot
-@dp.message_handler(state=None)
-async def bot_echo(message: types.Message):
-    await message.answer(message.text)
