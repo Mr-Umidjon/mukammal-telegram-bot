3204f981677ce7c3f06a19dcf5f6ffa0d5b922c8
Umidjon <mrumidjon2020@gmail.com>
2022-11-26 21:14:17 +0500

    states keyboards inline

--- 07-states-keyboards-inline/aiogram-bot-template-master/handlers/users/start.py
+++ 07-states-keyboards-inline/aiogram-bot-template-master/handlers/users/start.py
@@ -1,9 +1,16 @@
+import logging
+
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
+
+from keyboards.default.start_menu_keyboard import menu_start
 
 from loader import dp
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    logging.info(message)
+    logging.info(f"{message.from_user.id=}")
+    logging.info(f"{message.from_user.full_name=}")
+    await message.answer(f"Salom, {message.from_user.full_name}!", reply_markup=menu_start)
---
+++ 07-states-keyboards-inline/aiogram-bot-template-master/keyboards/default/__init__.py
@@ -0,0 +1,1 @@
+from . import start_menu_keyboard
