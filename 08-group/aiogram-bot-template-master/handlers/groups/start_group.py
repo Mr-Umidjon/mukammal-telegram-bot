2789f6f76fa5bc5a792a6a949b832431b7a1d285
Umidjon <mrumidjon2020@gmail.com>
2022-11-28 21:31:40 +0500

    groups

--- 08-group/aiogram-bot-template-master/handlers/groups/start_group.py
+++ 08-group/aiogram-bot-template-master/handlers/groups/start_group.py
@@ -1,11 +1,11 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
-from filters import IsPrivate
+from filters import IsGroup
 # from ...filters import IsPrivate
 # from ...filters import IsPrivate
 from loader import dp
 
 
-@dp.message_handler(IsPrivate(), CommandStart())
+@dp.message_handler(IsGroup(), CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    await message.answer(f"Salom, {message.from_user.full_name}, siz guruhdasiz!")
