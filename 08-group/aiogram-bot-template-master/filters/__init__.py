3bba9ea118ed0370fdde838575b9702525c7736d
Umidjon <mrumidjon2020@gmail.com>
2022-11-28 21:27:47 +0500

    groups

--- 08-group/aiogram-bot-template-master/filters/__init__.py
+++ 08-group/aiogram-bot-template-master/filters/__init__.py
@@ -1,9 +1,9 @@
-from aiogram import Dispatcher
-
 from loader import dp
-# from .is_admin import AdminFilter
-
+from .admins import AdminFilter
+from .group import IsGroup
+from .private_chat import IsPrivate
 
 if __name__ == "filters":
-    #dp.filters_factory.bind(is_admin)
-    pass
+    dp.filters_factory.bind(AdminFilter)
+    dp.filters_factory.bind(IsGroup)
+    dp.filters_factory.bind(IsPrivate)
