d062d9e4f4e810e3bfdbb50083302e66708fe826
Umidjon <mrumidjon2020@gmail.com>
2022-11-30 02:28:16 +0500

    channels

--- 09-channels/aiogram-bot-template-master/middlewares/__init__.py
+++ 09-channels/aiogram-bot-template-master/middlewares/__init__.py
@@ -2,7 +2,8 @@ from aiogram import Dispatcher
 
 from loader import dp
 from .throttling import ThrottlingMiddleware
-
+from .checksub import BigBrother
 
 if __name__ == "middlewares":
     dp.middleware.setup(ThrottlingMiddleware())
+    dp.middleware.setup(BigBrother())
