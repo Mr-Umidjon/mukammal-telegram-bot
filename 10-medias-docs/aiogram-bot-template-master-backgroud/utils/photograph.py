b8fbd9bfcf4665a4167fbaa00013e2e9a817d7cf
Umidjon <mrumidjon2020@gmail.com>
2022-12-02 19:50:01 +0500

    media

---
+++ 10-medias-docs/aiogram-bot-template-master-backgroud/utils/photograph.py
@@ -0,0 +1,15 @@
+from io import BytesIO
+import aiohttp
+from aiogram import types
+from ..loader import bot
+
+
+async def photo_link(photo: types.photo_size.PhotoSize):
+    with await photo.download(BytesIO()) as file:
+        form = aiohttp.FormData()
+        form.add_field(name='file', value=file, )
+        async with bot.session.post('https://telegra.ph/upload', data=form) as response:
+            img_src = await response.json()
+
+    link = "https://telegra.ph/" + img_src[0]['src']
+    return link
