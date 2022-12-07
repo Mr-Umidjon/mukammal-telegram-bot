6813225b0806646c09927cdf3888d809bb42c8e8
Umidjon <mrumidjon2020@gmail.com>
2022-12-08 00:01:22 +0500

    pavement

---
+++ 12-pavement/aiogram-bot-template-master/keyboards/inline/product_keys.py
@@ -0,0 +1,12 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def build_keyboards(product):
+    keys = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text='Xarid qilish', callback_data=f"product: {product}"),
+            ],
+        ],
+    )
+    return keys
