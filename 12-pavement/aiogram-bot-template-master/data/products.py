8d37775c8462b23ff43e2084df4ea386b68d268f
Umidjon <mrumidjon2020@gmail.com>
2022-12-07 15:24:00 +0500

    pavement

--- 12-pavement/aiogram-bot-template-master/data/products.py
+++ 12-pavement/aiogram-bot-template-master/data/products.py
@@ -49,3 +49,40 @@ python_book = Product(
     need_shipping_address=True,
     is_flexible=True,
 )
+
+REGULAR_SHIPPING = types.ShippingOption(
+    id="post_reg",
+    title="Fargo (3 kun)",
+    prices=[
+        LabeledPrice(
+            label="Maxsus quti",
+            amount=100,
+        ),
+        LabeledPrice(
+            label="3 ish kunida yetkazish",
+            amount=100,
+        )
+    ]
+)
+
+FAST_SHIPPING = types.ShippingOption(
+    id='post_fast',
+    title='Express pochta (1 kun)',
+    prices=[
+        LabeledPrice(
+            label='1 kunda yetkazish',
+            amount=100
+        ),
+    ]
+)
+
+PICKUP_SHIPPING = types.ShippingOption(
+    id='pickup',
+    title="Do'kondan olib ketish",
+    prices=[
+        LabeledPrice(
+            label="Yetkazib berishsiz",
+            amount=-100,
+        )
+    ]
+)
