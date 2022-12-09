324fd0f66a17dfc4b6fb0de333d7380d55aaf8b4
Umidjon <mrumidjon2020@gmail.com>
2022-12-10 03:06:22 +0500

    postgresql

--- 14-postgresql/aiogram-bot-template-master/handlers/users/start.py
+++ 14-postgresql/aiogram-bot-template-master/handlers/users/start.py
@@ -1,9 +1,24 @@
+import asyncpg.exceptions
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from loader import dp
+from loader import dp, db, bot
+from data.config import ADMINS
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
+    try:
+        user = await db.add_user(
+            telegram_id=message.from_user.id,
+            full_name=message.from_user.full_name,
+            username=message.from_user.username
+        )
+    except asyncpg.exceptions.UniqueViolationError:
+        user = await db.select_user(telegram_id=message.from_user.id)
     await message.answer(f"Salom, {message.from_user.full_name}!")
+
+    count = await db.count_users()
+    msg = f"{message.from_user.full_name} bazaga qo'shildi.\n" \
+          f"Bazada {count} ta foydalanuvchi bor"
+    await bot.send_message(chat_id=ADMINS[0], text=msg)
