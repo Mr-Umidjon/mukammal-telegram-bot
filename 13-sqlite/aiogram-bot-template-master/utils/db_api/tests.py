f820f3e72da3034712a3a5a328a710643a500e82
Umidjon <mrumidjon2020@gmail.com>
2022-12-10 01:13:59 +0500

    sqlite

---
+++ 13-sqlite/aiogram-bot-template-master/utils/db_api/tests.py
@@ -0,0 +1,20 @@
+from utils.db_api.sqlite import DataBase
+
+
+def test():
+    db = DataBase()
+    db.create_table_users()
+    db.add_user(1, "one", 'email')
+    db.add_user(2, "two", 'email')
+    db.add_user(3, "three", 'email')
+    db.add_user(4, "four", 'email')
+    db.add_user(5, "five", 'email')
+
+    users = db.select_all_users()
+    print(f"Barcha foydalanuvchilar: {users}")
+
+    user = db.select_user(Name="five", id=5)
+    print(f"Bitta foydalanuvchini ko'rish {user}")
+
+
+test()
