b47c3ea3b648255b622c0199e7c80e5593eee603
Umidjon <mrumidjon2020@gmail.com>
2022-12-10 03:01:05 +0500

    postgresql

--- 14-postgresql/aiogram-bot-template-master/app.py
+++ 14-postgresql/aiogram-bot-template-master/app.py
@@ -9,6 +9,8 @@ from utils.set_bot_commands import set_default_commands
 async def on_startup(dispatcher):
     await db.create()
 
+    await db.drop_users()
+
     await db.create_table_users()
 
     # Birlamchi komandalar (/star va /help)
