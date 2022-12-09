9982b175ae76f320a4ec88f641b83d889e159931
Umidjon <mrumidjon2020@gmail.com>
2022-12-10 02:13:05 +0500

    postgresql

--- 14-postgresql/aiogram-bot-template-master/utils/db_api/postgresql.py
+++ 14-postgresql/aiogram-bot-template-master/utils/db_api/postgresql.py
@@ -7,3 +7,34 @@ from asyncpg.pool import Pool
 from ...data import config
 
 
+class DataBase:
+
+    def __init__(self):
+        self.pool: Union[Pool, None] = None
+
+    async def create(self):
+        self.pool = await asyncpg.create_pool(
+            user=config.DB_USER,
+            password=config.DB_PASS,
+            host=config.DB_HOST,
+            database=config.DB_NAME,
+        )
+
+    async def execute(self, command, *args,
+                      fetch: bool = False,
+                      fetchval: bool = False,
+                      fetchrow: bool = False,
+                      execute: bool = False,
+                      ):
+        async with self.pool.acquire() as connection:
+            connection: Connection
+            async with connection.transaction():
+                if fetch:
+                    result = await connection.fetch(command, *args)
+                elif fetchval:
+                    result = await connection.fetchval(command, *args)
+                elif fetchrow:
+                    result = await connection.fetchrow(command, *args)
+                elif execute:
+                    result = await connection.execute(command, *args)
+            return result
