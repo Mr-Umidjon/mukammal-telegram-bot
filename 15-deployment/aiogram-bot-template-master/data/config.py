0a9418d3452c3c12da287fe61c82ed684a569ee4
Umidjon <mrumidjon2020@gmail.com>
2022-12-13 23:26:29 +0500

    deployment

--- 15-deployment/aiogram-bot-template-master/data/config.py
+++ 15-deployment/aiogram-bot-template-master/data/config.py
@@ -1,10 +1,17 @@
-from environs import Env
+# from environs import Env
+#
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+#
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+import os
+
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+ADMINS = list(os.environ.get("ADMINS"))
+IP = str(os.environ.get("ip"))
