f9c8e28d94845ff8671996daf0624971a34bf2d0
Umidjon <mrumidjon2020@gmail.com>
2022-12-13 23:18:40 +0500

    deployment

--- 15-deployment/aiogram-bot-template-master/handlers/users/help.py
+++ 15-deployment/aiogram-bot-template-master/handlers/users/help.py
@@ -4,6 +4,7 @@ from aiogram.dispatcher.filters.builtin import CommandHelp
 from loader import dp
 
 
+
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
     text = ("Buyruqlar: ",
