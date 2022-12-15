c6d43d3e82cf56e02e1534cfd1cccafecbf4bcc2
Umidjon <mrumidjon2020@gmail.com>
2022-12-15 23:51:03 +0500

    ebozor

--- 16-ebozorbot/aiogram-bot-template-master/utils/db_api/db_commands.py
+++ 16-ebozorbot/aiogram-bot-template-master/utils/db_api/db_commands.py
@@ -4,11 +4,10 @@ import asyncpg
 from asyncpg import Connection
 from asyncpg.pool import Pool
 
-from ...data import config
+from data import config
 
 
 class Database:
-
     def __init__(self):
         self.pool: Union[Pool, None] = None
 
@@ -20,12 +19,15 @@ class Database:
             database=config.DB_NAME,
         )
 
-    async def execute(self, command, *args,
-                      fetch: bool = False,
-                      fetchval: bool = False,
-                      fetchrow: bool = False,
-                      execute: bool = False,
-                      ):
+    async def execute(
+            self,
+            command,
+            *args,
+            fetch: bool = False,
+            fetchval: bool = False,
+            fetchrow: bool = False,
+            execute: bool = False,
+    ):
         async with self.pool.acquire() as connection:
             connection: Connection
             async with connection.transaction():
@@ -52,8 +54,34 @@ class Database:
 
     @staticmethod
     def format_args(sql, parameters: dict):
-        sql += " AND ".join([
-            f"{item} = ${num}" for num, item in enumerate(parameters.keys(),
-                                                          start=1)
-        ])
+        sql += " AND ".join(
+            [f"{item} = ${num}" for num, item in enumerate(parameters.keys(), start=1)]
+        )
         return sql, tuple(parameters.values())
+
+    async def add_user(self, full_name, username, telegram_id):
+        sql = "INSERT INTO users (full_name, username, telegram_id) VALUES($1, $2, $3) returning *"
+        return await self.execute(sql, full_name, username, telegram_id, fetchrow=True)
+
+    async def select_all_users(self):
+        sql = "SELECT * FROM Users"
+        return await self.execute(sql, fetch=True)
+
+    async def select_user(self, **kwargs):
+        sql = "SELECT * FROM Users WHERE "
+        sql, parameters = self.format_args(sql, parameters=kwargs)
+        return await self.execute(sql, *parameters, fetchrow=True)
+
+    async def count_users(self):
+        sql = "SELECT COUNT(*) FROM Users"
+        return await self.execute(sql, fetchval=True)
+
+    async def update_user_username(self, username, telegram_id):
+        sql = "UPDATE Users SET username=$1 WHERE telegram_id=$2"
+        return await self.execute(sql, username, telegram_id, execute=True)
+
+    async def delete_users(self):
+        await self.execute("DELETE FROM Users WHERE TRUE", execute=True)
+
+    async def drop_users(self):
+        await self.execute("DROP TABLE Users", execute=True)
