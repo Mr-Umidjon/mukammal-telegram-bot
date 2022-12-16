b69776021cae59fdc13403ec6ccf0d8bf9335047
Umidjon <mrumidjon2020@gmail.com>
2022-12-16 10:02:41 +0500

    ebozorbot

--- 16-ebozorbot/aiogram-bot-template-master/keyboards/inline/menu_keyboards.py
+++ 16-ebozorbot/aiogram-bot-template-master/keyboards/inline/menu_keyboards.py
@@ -4,3 +4,12 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
 
 from ...loader import db
+
+menu_cd = CallbackData('show_menu', 'level', 'category', 'subcategory', 'item_id')
+buy_item = CallbackData('buy', 'item_id')
+
+
+def make_callback_data(level, category='0', subcategory='0', item_id='0'):
+    return menu_cd.new(level=level, category=category, subcategory=subcategory, item_id=item_id)
+
+
