04797bd3e77a040a9f71f6c3ad38ebed67d65cf7
Umidjon <mrumidjon2020@gmail.com>
2022-12-16 10:09:58 +0500

    ebozorbot

--- 16-ebozorbot/aiogram-bot-template-master/keyboards/inline/menu_keyboards.py
+++ 16-ebozorbot/aiogram-bot-template-master/keyboards/inline/menu_keyboards.py
@@ -13,3 +13,13 @@ def make_callback_data(level, category='0', subcategory='0', item_id='0'):
     return menu_cd.new(level=level, category=category, subcategory=subcategory, item_id=item_id)
 
 
+async def categories_keyboard():
+    CURRENT_LEVEL = 0
+
+    markup = InlineKeyboardMarkup(row_width=1)
+
+    categories = db.get_categories()
+
+    for category in categories:
+
+        number_of_items = await db.count_products(category['category_code'])
