131027c9960183e9ee2d79645a4c5fd0ad1b060f
Umidjon <mrumidjon2020@gmail.com>
2022-12-17 19:24:45 +0500

    admin panel

--- 17-admin-panel/aiogram-bot-template-master/ebozor/products/models.py
+++ 17-admin-panel/aiogram-bot-template-master/ebozor/products/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    full_name = models.CharField(verbose_name="Ism", max_length=100)
+    username = models.CharField(verbose_name='Telegram username', max_length=100, null=True)
+    telegram_id = models.BigIntegerField(verbose_name='Telegram ID', unique=True, default=1)
+    email = models.CharField(verbose_name="Email", max_length=50, null=True)
